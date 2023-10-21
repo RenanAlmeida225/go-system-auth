b0f1fdaeee0a28f5bcb0d3c8534e00c52b5bf3c2
italo renan <italorenan225@gmail.com>
2023-10-21 07:55:56 -0300

    feat: add initialization of handlers

--- router/routes.go
+++ router/routes.go
@@ -1,12 +1,16 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/RenanAlmeida225/go-system-auth/handler"
+	"github.com/gin-gonic/gin"
+)
 
 func InitializeRoutes(r *gin.Engine) {
+	handler.InitializeHandler()
 	v1 := r.Group("/api/v1")
 	auth := v1.Group("auth")
 	{
-		auth.POST("/signUp")
+		auth.POST("/signUp", handler.SignUp)
 		auth.POST("/signIn")
 	}
 }
