2b25d95a34db9f9ca950fcb5a40395d7e6346e84
italo renan <italorenan225@gmail.com>
2023-10-21 07:53:00 -0300

    feat: config init

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/RenanAlmeida225/go-system-auth/config"
 	"github.com/RenanAlmeida225/go-system-auth/router"
 )
 
 func main() {
+	if err := config.Init(); err != nil {
+		panic(err)
+	}
 	router.Initialize()
 }
