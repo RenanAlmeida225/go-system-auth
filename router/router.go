7b0b8a2985629e04dfac0e468e39d89c092ac567
italo renan <italorenan225@gmail.com>
2023-10-21 06:45:24 -0300

    feat: initialize routes

--- router/router.go
+++ router/router.go
@@ -4,10 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func Initialize() {
 	r := gin.Default()
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "hello, world",
-		})
-	})
+	InitializeRoutes(r)
 	r.Run()
 }
