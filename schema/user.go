a4ed483d087ef90d843fbf658c243180913d1a77
italo renan <italorenan225@gmail.com>
2023-10-21 15:14:22 -0300

    feat: add relationship has one

--- schema/user.go
+++ schema/user.go
@@ -3,10 +3,12 @@ package schema
 import "time"
 
 type User struct {
-	ID        uint `gorm:"primarykey"`
-	Username  string
-	Email     string `gorm:"unique"`
-	Password  string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID           uint `gorm:"primarykey"`
+	Username     string
+	Email        string `gorm:"unique"`
+	Password     string
+	IsEnabled    bool         `gorm:"default:false"`
+	Confirmation Confirmation `gorm:"foreignKey:Email;references:email"`
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
 }
